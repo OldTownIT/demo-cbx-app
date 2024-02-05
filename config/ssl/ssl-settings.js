1be15f0bb163d0b81a4a0cd8552cd8798fad4145
mattsahr <mattsahr@gmail.com>
2024-02-05 13:40:06 +0100

    Remove static ssl settings

--- config/ssl/ssl-settings.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const sslSettings = {
-    key: fs.readFileSync('../openSSL/demo_ssl.key'),
-    cert: fs.readFileSync('../openSSL/demo_ssl.crt'),
-};
-
-export default sslSettings;
